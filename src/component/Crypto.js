29ac50633382c57a9b5ca5f1e7b958bcf4bc600c
Debanjana <debanjana.9806@gmail.com>
2022-11-26 13:42:14 -0800

    initial commit

---
+++ src/component/Crypto.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import {Container, Table} from 'react-bootstrap'
+const Crypto = () =>{
+const [cryptodata,setData] =useState([])
+
+useEffect(() => {
+   axios
+   .get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+   .then((res)=>{setData(res.data)})
+   .catch((err)=>{console.log(err)})
+}, [])
+  return (
+    <Container fluid='md'>
+         <Table striped bordered hover variant="dark" className="mt-2 mb-2">
+          {cryptodata.map((datas)=>(
+           
+                <tbody key={datas.ath}>
+                    <tr>
+                    <td>{datas.name}</td>
+                    <td>{datas.id}</td>
+                    <td><img src={datas.image} style={{height:'20px'}} alt="icons"/></td>
+                    <td>{datas.symbol}</td>
+                    <td>{datas.current_price}</td>
+                    <td>{datas.total_volume}</td>
+                    </tr>
+                </tbody>
+          
+        ))}
+        </Table>
+       
+    </Container>
+  )
+}
+
+export default Crypto
